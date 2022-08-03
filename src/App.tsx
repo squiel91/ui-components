3d6cacb9ebe22896d8b8ecbaad31031708656b2a
Ezequiel Sanchez <squiel91@gmail.com>
2022-08-03 12:59:27 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NNButton from './components/NNButton.ui';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <NNButton />
         </a>
       </header>
     </div>
---
+++ src/components/NNButton.ui.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react"
+
+const NNButton: FC = () => (
+  <button className="nn-button">
+    Soy un nocnoc button
+  </button>
+)
+
+export default NNButton
